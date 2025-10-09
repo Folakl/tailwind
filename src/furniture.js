1a6ab7de362810aef8a5a13ebecc4c0a9f53a29e
Yakub Roimot Omotara <yakubroimot2001@gmail.com>
2025-10-09 11:03:15 +0100

    update

--- src/furniture.js
+++ src/furniture.js
@@ -3,16 +3,16 @@ function brandInput(){
 }
 function navigateTo(page){
     if (page === "home" ){
-    window.location.href = "index.html"
+    window.location.href = "/Index.html"
     }
     else if (page === "store"){
-        window.location.href = "brand.html"
+        window.location.href = "/src/brand.html"
     }
     else if (page === "brand"){
-        window.location.href = "product.html"
+        window.location.href = "/src/product.html"
     }
     else if (page === "pages"){
-        window.location.href = "cart.html"
+        window.location.href = "/src/cart.html"
     }
     else{
         alert("page not found")
